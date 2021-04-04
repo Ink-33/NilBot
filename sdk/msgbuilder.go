878a939aedae4f90d823629b0a83e8042b1e0ad4
Ink33 <Ink33@smlk.org>
2021-04-04 23:42:18 +0800

    update

--- sdk/msgbuilder.go
+++ sdk/msgbuilder.go
@@ -16,7 +16,7 @@ type MsgArray []MsgElement
 
 // MsgBuilder is a builder used to build array messgae.
 type MsgBuilder struct {
-	msgMap MsgArray
+	msg MsgArray
 }
 
 // MsgBuilderArg is used to represent complex parameters in the message element.
@@ -27,31 +27,31 @@ type MsgBuilderArg interface{}
 
 // At append an at message element to message array.
 func (m *MsgBuilder) At(qq string) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "at", "data": MsgElement{"qq": qq}})
+	m.msg = append(m.msg, MsgElement{"type": "at", "data": MsgElement{"qq": qq}})
 	return m
 }
 
 // CardImage append a CardImage message element to message array.
 func (m *MsgBuilder) CardImage(args MsgBuilderArg) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "cardimage", "data": args})
+	m.msg = append(m.msg, MsgElement{"type": "cardimage", "data": args})
 	return m
 }
 
 // Face append a face message element to message array.
 func (m *MsgBuilder) Face(id string) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "face", "data": MsgElement{"id": id}})
+	m.msg = append(m.msg, MsgElement{"type": "face", "data": MsgElement{"id": id}})
 	return m
 }
 
 // Forward append a forward message element to message array.
 func (m *MsgBuilder) Forward(id string) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "forward", "data": MsgElement{"id": id}})
+	m.msg = append(m.msg, MsgElement{"type": "forward", "data": MsgElement{"id": id}})
 	return m
 }
 
 // Gife append an gift message element to message array.
 func (m *MsgBuilder) Gift(qq, giftID string) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "gift", "data": MsgElement{"qq": qq, "id": giftID}})
+	m.msg = append(m.msg, MsgElement{"type": "gift", "data": MsgElement{"qq": qq, "id": giftID}})
 	return m
 }
 
@@ -59,24 +59,24 @@ func (m *MsgBuilder) Gift(qq, giftID string) *MsgBuilder {
 func (m *MsgBuilder) Image(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "image", "data": args})
+		m.msg = append(m.msg, MsgElement{"type": "image", "data": args})
 	case string:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "image", "data": MsgElement{"file": args}})
+		m.msg = append(m.msg, MsgElement{"type": "image", "data": MsgElement{"file": args}})
 	default:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
+		m.msg = append(m.msg, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
 	}
 	return m
 }
 
 // JSON append a JSON message element to message array.
 func (m *MsgBuilder) JSON(json string) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "json", "data": MsgElement{"data": json}})
+	m.msg = append(m.msg, MsgElement{"type": "json", "data": MsgElement{"data": json}})
 	return m
 }
 
 // Music append a music message element to message array.
 func (m *MsgBuilder) Music(args MsgBuilderArg) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "music", "data": args})
+	m.msg = append(m.msg, MsgElement{"type": "music", "data": args})
 	return m
 }
 
@@ -86,18 +86,18 @@ func (m *MsgBuilder) Music(args MsgBuilderArg) *MsgBuilder {
 func (m *MsgBuilder) Node(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "node", "data": args})
+		m.msg = append(m.msg, MsgElement{"type": "node", "data": args})
 	case string:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "node", "data": MsgElement{"id": args}})
+		m.msg = append(m.msg, MsgElement{"type": "node", "data": MsgElement{"id": args}})
 	default:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
+		m.msg = append(m.msg, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
 	}
 	return m
 }
 
 // Poke append an Poke message element to message array.
 func (m *MsgBuilder) Poke(qq string) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "poke", "data": MsgElement{"qq": qq}})
+	m.msg = append(m.msg, MsgElement{"type": "poke", "data": MsgElement{"qq": qq}})
 	return m
 }
 
@@ -105,11 +105,11 @@ func (m *MsgBuilder) Poke(qq string) *MsgBuilder {
 func (m *MsgBuilder) Record(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "record", "data": args})
+		m.msg = append(m.msg, MsgElement{"type": "record", "data": args})
 	case string:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "record", "data": MsgElement{"file": args}})
+		m.msg = append(m.msg, MsgElement{"type": "record", "data": MsgElement{"file": args}})
 	default:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
+		m.msg = append(m.msg, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
 	}
 	return m
 }
@@ -118,18 +118,18 @@ func (m *MsgBuilder) Record(args MsgBuilderArg) *MsgBuilder {
 func (m *MsgBuilder) Share(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "share", "data": args})
+		m.msg = append(m.msg, MsgElement{"type": "share", "data": args})
 	case string:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "share", "data": MsgElement{"url": args}})
+		m.msg = append(m.msg, MsgElement{"type": "share", "data": MsgElement{"url": args}})
 	default:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
+		m.msg = append(m.msg, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
 	}
 	return m
 }
 
 // Text append a text message element to message array.
 func (m *MsgBuilder) Text(text string) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "text", "data": MsgElement{"text": text}})
+	m.msg = append(m.msg, MsgElement{"type": "text", "data": MsgElement{"text": text}})
 	return m
 }
 
@@ -137,26 +137,42 @@ func (m *MsgBuilder) Text(text string) *MsgBuilder {
 func (m *MsgBuilder) Vedio(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "vedio", "data": args})
+		m.msg = append(m.msg, MsgElement{"type": "vedio", "data": args})
 	case string:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "vedio", "data": MsgElement{"file": args}})
+		m.msg = append(m.msg, MsgElement{"type": "vedio", "data": MsgElement{"file": args}})
 	default:
-		m.msgMap = append(m.msgMap, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
+		m.msg = append(m.msg, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
 	}
 	return m
 }
 
 // XML append a XML message element to message array.
 func (m *MsgBuilder) XML(xml string) *MsgBuilder {
-	m.msgMap = append(m.msgMap, MsgElement{"type": "xml", "data": MsgElement{"data": xml}})
+	m.msg = append(m.msg, MsgElement{"type": "xml", "data": MsgElement{"data": xml}})
 	return m
 }
 
-// Build marshal the message array to []byte.
-func (m *MsgBuilder) Build() ([]byte, error) {
-	b, err := json.Marshal(m.msgMap)
+// Append a MsgElement to MsgBuilder.
+func (m *MsgBuilder) Append(e MsgElement) *MsgBuilder {
+	m.msg = append(m.msg, e)
+	return m
+}
+
+// AppendArray to MsgBuilder.
+func (m *MsgBuilder) AppendArray(a MsgArray) *MsgBuilder {
+	m.msg = append(m.msg, a...)
+	return m
+}
+
+// Marshal message array to []byte.
+func (m *MsgBuilder) Marshal() ([]byte, error) {
+	b, err := json.Marshal(m.msg)
 	if err != nil {
 		return nil, err
 	}
 	return b, nil
+}
+
+func (m *MsgBuilder) GetArray() MsgArray {
+	return m.msg
 }
