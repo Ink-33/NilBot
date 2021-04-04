e76a066325ee7332dad39d9a6855b3c828b522dc
Ink33 <Ink33@smlk.org>
2021-04-04 18:55:18 +0800

    update message Builder and parser

--- sdk/msgbuilder.go
+++ sdk/msgbuilder.go
@@ -11,14 +11,29 @@ var json = jsoniter.ConfigFastest
 // MsgElement is an element of array message.
 type MsgElement map[string]interface{}
 
+// MsgArray contains MsgElements, used to send and revice message by using OneBot array message format.
+type MsgArray []MsgElement
+
 // MsgBuilder is a builder used to build array messgae.
 type MsgBuilder struct {
-	msgMap []interface{}
+	msgMap MsgArray
 }
+
+// MsgBuilderArg is used to represent complex parameters in the message element.
+// Usually if the function that can recive more than one parameter need to use this.
+//
+// For more detail, please read document.
+type MsgBuilderArg interface{}
 
 // At append an at message element to message array.
 func (m *MsgBuilder) At(qq string) *MsgBuilder {
 	m.msgMap = append(m.msgMap, MsgElement{"type": "at", "data": MsgElement{"qq": qq}})
+	return m
+}
+
+// CardImage append a CardImage message element to message array.
+func (m *MsgBuilder) CardImage(args MsgBuilderArg) *MsgBuilder {
+	m.msgMap = append(m.msgMap, MsgElement{"type": "cardimage", "data": args})
 	return m
 }
 
@@ -28,8 +43,20 @@ func (m *MsgBuilder) Face(id string) *MsgBuilder {
 	return m
 }
 
+// Forward append a forward message element to message array.
+func (m *MsgBuilder) Forward(id string) *MsgBuilder {
+	m.msgMap = append(m.msgMap, MsgElement{"type": "forward", "data": MsgElement{"id": id}})
+	return m
+}
+
+// Gife append an gift message element to message array.
+func (m *MsgBuilder) Gift(qq, giftID string) *MsgBuilder {
+	m.msgMap = append(m.msgMap, MsgElement{"type": "gift", "data": MsgElement{"qq": qq, "id": giftID}})
+	return m
+}
+
 // Image append an image message element to message array.
-func (m *MsgBuilder) Image(args interface{}) *MsgBuilder {
+func (m *MsgBuilder) Image(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
 		m.msgMap = append(m.msgMap, MsgElement{"type": "image", "data": args})
@@ -41,14 +68,41 @@ func (m *MsgBuilder) Image(args interface{}) *MsgBuilder {
 	return m
 }
 
+// JSON append a JSON message element to message array.
+func (m *MsgBuilder) JSON(json string) *MsgBuilder {
+	m.msgMap = append(m.msgMap, MsgElement{"type": "json", "data": MsgElement{"data": json}})
+	return m
+}
+
 // Music append a music message element to message array.
-func (m *MsgBuilder) Music(args interface{}) *MsgBuilder {
+func (m *MsgBuilder) Music(args MsgBuilderArg) *MsgBuilder {
 	m.msgMap = append(m.msgMap, MsgElement{"type": "music", "data": args})
 	return m
 }
 
+// Node append a forward node element to message array.
+//
+// You should read document before using this.
+func (m *MsgBuilder) Node(args MsgBuilderArg) *MsgBuilder {
+	switch args.(type) {
+	case MsgElement:
+		m.msgMap = append(m.msgMap, MsgElement{"type": "node", "data": args})
+	case string:
+		m.msgMap = append(m.msgMap, MsgElement{"type": "node", "data": MsgElement{"id": args}})
+	default:
+		m.msgMap = append(m.msgMap, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
+	}
+	return m
+}
+
+// Poke append an Poke message element to message array.
+func (m *MsgBuilder) Poke(qq string) *MsgBuilder {
+	m.msgMap = append(m.msgMap, MsgElement{"type": "poke", "data": MsgElement{"qq": qq}})
+	return m
+}
+
 // Record append a record message element to message array.
-func (m *MsgBuilder) Record(args interface{}) *MsgBuilder {
+func (m *MsgBuilder) Record(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
 		m.msgMap = append(m.msgMap, MsgElement{"type": "record", "data": args})
@@ -61,7 +115,7 @@ func (m *MsgBuilder) Record(args interface{}) *MsgBuilder {
 }
 
 // Share append an share message element to message array.
-func (m *MsgBuilder) Share(args interface{}) *MsgBuilder {
+func (m *MsgBuilder) Share(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
 		m.msgMap = append(m.msgMap, MsgElement{"type": "share", "data": args})
@@ -80,7 +134,7 @@ func (m *MsgBuilder) Text(text string) *MsgBuilder {
 }
 
 // Vedio append a vedio message element to message array.
-func (m *MsgBuilder) Vedio(args interface{}) *MsgBuilder {
+func (m *MsgBuilder) Vedio(args MsgBuilderArg) *MsgBuilder {
 	switch args.(type) {
 	case MsgElement:
 		m.msgMap = append(m.msgMap, MsgElement{"type": "vedio", "data": args})
@@ -89,6 +143,12 @@ func (m *MsgBuilder) Vedio(args interface{}) *MsgBuilder {
 	default:
 		m.msgMap = append(m.msgMap, MsgElement{"type": "text", "data": MsgElement{"text": fmt.Sprintf("Unexpected args type: %T", args)}})
 	}
+	return m
+}
+
+// XML append a XML message element to message array.
+func (m *MsgBuilder) XML(xml string) *MsgBuilder {
+	m.msgMap = append(m.msgMap, MsgElement{"type": "xml", "data": MsgElement{"data": xml}})
 	return m
 }
 
---
+++ sdk/msgparser.go
@@ -0,0 +1,45 @@
+package nilbot
+
+import "sync"
+
+// ParsedMessageElement is
+type ParsedMessageElement struct {
+	data MsgElement
+}
+
+// MsgParser is used to parser message of array format.
+func MsgParser(message []byte) ([]ParsedMessageElement, error) {
+	var pm = &[]MsgElement{}
+	err := json.Unmarshal(message, pm)
+	if err != nil {
+		return nil, err
+	}
+	l := len(*pm)
+	var pme = make([]ParsedMessageElement, l)
+	wg := sync.WaitGroup{}
+	wg.Add(l)
+	for i := 0; i < l; i++ {
+		go func(i int, pm []MsgElement) {
+			pme[i].data = pm[i]
+			wg.Done()
+		}(i, *pm)
+	}
+	wg.Wait()
+	return pme, nil
+}
+
+func (e *ParsedMessageElement) Get(key string) string {
+	r := e.data["data"].(map[string]interface{})[key]
+	if r != nil {
+		return r.(string)
+	}
+	return ""
+}
+
+func (e *ParsedMessageElement) GetElement() MsgElement {
+	return e.data
+}
+
+func (e *ParsedMessageElement) Type() string {
+	return e.data["type"].(string)
+}
